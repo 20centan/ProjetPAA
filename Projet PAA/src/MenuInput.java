76e6df5b3c5bf2a6a92cf976794dd9e20b965106
Lylichen07 <feedpoipoi@gmail.com>
2024-11-01 17:50:54 +0100

    create class to get Menu input

---
+++ Projet PAA/src/MenuInput.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+
+
+public class MenuInput extends UserInput{
+    public MenuInput(Scanner sc){
+        super(sc);
+    }
+
+    public char saisirColon(){
+        char character;
+        
+        while(true){
+            character = saisirChar("Entrez un colon: ", "Erreur - Veuillez entrez un lettre.");
+            character = Character.toUpperCase(character);
+
+            if(character == 'A'){
+                break;
+            }
+            System.out.println("Erreur - Entrez un colon existant");
+        }
+
+        return character;
+    }
+
+    public String [] saisirPreferences(Colonie colonie){
+        String [] colon_preferences;
+    
+        while(true){
+            colon_preferences = saisirSuite("[a-zA-Z](\\s+\\d+)+");
+
+            if(colonie.appartientColonie(colon_preferences[0].charAt(0))){
+                System.out.println("Erreur - Entrez un colon existant.");
+                continue;
+            } 
+            if(colon_preferences.length - 1 != colonie.getNbColons()){
+                System.out.println("Erreur - Entrez le bon nombre de préférence.");
+                continue;
+            }
+            break;
+        }
+        
+        return colon_preferences;
+    }
+
+    public char [] saisirRelation(Colonie colonie){
+        String [] colon_colon;
+
+        while(true){
+            colon_colon = saisirSuite("[a-zA-Z]\\s+[a-zA-Z]");
+
+            if(colonie.appartientColonie(colon_colon[0].charAt(0)) ||
+            colonie.appartientColonie(colon_colon[1].charAt(0))){
+                System.out.println("Erreur - Entrez des colons existants.");
+                continue;
+            }  
+           
+            if(colon_colon[0] == colon_colon[1]){
+                System.out.println("Erreur - Entrez des colons distincts.");
+                continue;
+            } 
+           
+            break;
+        }
+
+        return new char[] {colon_colon[0].charAt(0), colon_colon[1].charAt(0)};
+    }
+}
