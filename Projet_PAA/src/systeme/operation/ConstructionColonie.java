266e5cf9ecd2e2a9f22110000d569945756b5246
Lylichen07 <feedpoipoi@gmail.com>
2024-12-05 10:49:12 +0100

    debug

--- Projet_PAA/src/systeme/operation/ConstructionColonie.java
+++ Projet_PAA/src/systeme/operation/ConstructionColonie.java
@@ -47,7 +47,7 @@ public abstract class ConstructionColonie {
 
         Ressource [] preferences = new Ressource[colon_preferences.length - 1];
 
-        for(int i = 0; i < colon_preferences.length; i++){
+        for(int i = 0; i < colon_preferences.length - 1; i++){
             preferences[i] = colonie.getRessource(colon_preferences[i + 1]); 
         }
 
