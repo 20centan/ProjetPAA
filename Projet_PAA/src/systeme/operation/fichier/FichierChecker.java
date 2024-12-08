c8e85036084649a0577215e47f4738973775fd28
Lylichen07 <feedpoipoi@gmail.com>
2024-12-08 15:28:43 +0100

    add comment

--- Projet_PAA/src/systeme/operation/fichier/FichierChecker.java
+++ Projet_PAA/src/systeme/operation/fichier/FichierChecker.java
@@ -234,8 +234,7 @@ public class FichierChecker{
 
 
     public void checkColonPreference(){
-        // vérifier le bon nombre de préférence/ tous les colons ont une préférence
-
+        // vérifier que tous les colons ont une préférence
     }
 
     public void checkSyntaxe(String ligne, String regex) throws FichierException{
